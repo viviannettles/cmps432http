7e612db2520a2784b7d2848240658173388e7960
Vivian Nettles <C00277545@louisiana.edu>
2021-03-31 14:53:15 -0500

    Final push
    Okay! the code is finished. In order to search a keyword, you'll want to add to the end of the URL "/{word to search}". For example:
    
    http://localhost:8080/cmps432http/rest/http/energy

--- RESTClient/src/main/java/net/codejava/ws/HelloClient.java
+++ RESTClient/src/main/java/net/codejava/ws/HelloClient.java
@@ -10,13 +10,13 @@ import org.glassfish.jersey.client.ClientConfig;
 public class HelloClient {
 
 	public static void main(String[] args) {
-		String uri = "http://localhost:8080/cmps432http/rest/bonjour";
+		String uri = "http://localhost:8080/cmps432http/rest/http";
 		ClientConfig config = new ClientConfig();
 		Client client = ClientBuilder.newClient(config);
 		WebTarget target = client.target(uri);
 		//String response = target.request().accept(MediaType.TEXT_HTML).get(String.class);
-		//String response = target.request().accept(MediaType.TEXT_PLAIN).get(String.class);
-		String response = target.request().accept(MediaType.APPLICATION_JSON).get(String.class);
+		String response = target.request().accept(MediaType.TEXT_PLAIN).get(String.class);
+		//String response = target.request().accept(MediaType.APPLICATION_JSON).get(String.class);
 		
 		System.out.println(response);
 	}
